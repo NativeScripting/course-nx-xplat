bb368ad03296e8fe1baa57821c393c6dc05bf94c
Alexander Ziskind <alex@nuvious.com>
2020-10-20 11:05:14 -0400

    implemented task list service

--- libs/features/tasklist/services/tasklist.service.ts
+++ libs/features/tasklist/services/tasklist.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@angular/core';
+import { TaskItem } from '../models/task-item.model';
 
 @Injectable({providedIn: 'root'})
 export class TaskListService {
-    constructor() { }
+
+    private taskList: TaskItem[] = [
+        {title: 'install Node'},
+        {title: 'install Nx CLI'},
+        {title: 'create new app'},
+        {title: 'serve app'},
+        {title: 'develop app'},
+        {title: 'deploy app'},
+      ];
     
+      getTaskList() {
+        return this.taskList;
+      }
 }
