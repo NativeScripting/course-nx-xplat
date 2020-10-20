a5b914dbee2dd1505988d839710970b89d0df912
Alexander Ziskind <alex@nuvious.com>
2020-10-20 13:41:36 -0400

    using configuration for web

--- apps/nativescript-tasks/src/app.module.ts
+++ apps/nativescript-tasks/src/app.module.ts
@@ -1,6 +1,10 @@
 // angular
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
+// libs
+import { environment } from '@nuvious/core';
+import {ENVIRONMENT_CONFIGURATION_TOKEN} from '@nuvious/core/environments/environment-configuration-token';
+
 // app
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './features/shared/shared.module';
@@ -11,6 +15,12 @@ import { AppComponent } from './app.component';
   imports: [CoreModule, SharedModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: ENVIRONMENT_CONFIGURATION_TOKEN,
+      useValue: environment
+    }
+  ],
   schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule {}
